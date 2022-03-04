71d435e867fff85b30609430b23a0e3abe243827
zakino123 <15rb076@ms.dendai.ac.jp>
2022-03-04 14:47:14 +0900

    devise-migrationファイル解釈

--- db/migrate/20220303084254_devise_create_users.rb
+++ db/migrate/20220303084254_devise_create_users.rb
@@ -3,17 +3,21 @@
 class DeviseCreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
+      ## DBに保存するパスワードの暗号化
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
+      ## パスワードリセット
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
 
+      ## cookieにログイン情報保持
       ## Rememberable
       t.datetime :remember_created_at
 
+      ## サインイン回数・時刻・IPアドレスを保存
       ## Trackable
       t.integer  :sign_in_count, default: 0, null: false
       t.datetime :current_sign_in_at
@@ -21,12 +25,14 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
       t.string   :current_sign_in_ip
       t.string   :last_sign_in_ip
 
+      ## メールによる登録確認
       ## Confirmable
       t.string   :confirmation_token
       t.datetime :confirmed_at
       t.datetime :confirmation_sent_at
       t.string   :unconfirmed_email # Only if using reconfirmable
 
+      ## 一定回数ログインに失敗によるアカウントロック
       ## Lockable
       t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
       t.string   :unlock_token # Only if unlock strategy is :email or :both
