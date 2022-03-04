bb3fec1e8cac821ee13f8749e6f05d1165a8fe05
zakino123 <15rb076@ms.dendai.ac.jp>
2022-03-04 11:53:12 +0900

    entrypoint.shの解釈

--- forDocker/rails/entrypoint.sh
+++ forDocker/rails/entrypoint.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+# bashの設定を確認変更コマンド(-e:パイプやサブジェルで実行したコマンドがエラーになったらシェルを修了)
 set -e
 
 # Remove a potentially pre-existing server.pid for Rails.
+# サーバーはDockerで作成しているため、Railsのserverがあるとエラーが起こる。
 rm -f /app/tmp/pids/server.pid
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
+# シェルスクリプトの変数を全て展開させる
 exec "$@"
