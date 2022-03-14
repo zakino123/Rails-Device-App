d68344446c77e7775b25fdef8fef8100d202300f
zakino123 <15rb076@ms.dendai.ac.jp>
2022-03-14 11:28:01 +0900

    DeviseとLINEログインの設定

--- Gemfile
+++ Gemfile
@@ -26,6 +26,12 @@ gem 'devise'
 
 gem 'devise-i18n'
 
+gem 'omniauth-line'
+
+gem 'omniauth-rails_csrf_protection'
+
+gem 'dotenv-rails'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :trackable, :confirmable, :lockable, :timeoutable
+         :trackable, :confirmable, :lockable, :timeoutable,
+         :omniauthable, omniauth_providers: %i[line]
 end
