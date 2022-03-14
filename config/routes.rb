452171a81454164a091afaf33343078675d75e1a
zakino123 <15rb076@ms.dendai.ac.jp>
2022-03-14 10:27:51 +0900

    アカウントメール確認画面デザイン反映

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => {
-    :confirmations => 'user/confirmations',
+    :confirmations => 'users/confirmations',
     :registrations => 'users/registrations',
     :sessions => 'users/sessions',
     :passwords => 'users/passwords'
